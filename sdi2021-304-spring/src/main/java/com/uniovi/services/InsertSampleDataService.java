ef7933364de1ac683806b51bef6239f74180d3ba
UO271245 <71383208+UO271245@users.noreply.github.com>
2021-02-28 20:27:11 +0100

    SDI-2021-304-4.2-Roles.

--- sdi2021-304-spring/src/main/java/com/uniovi/services/InsertSampleDataService.java
+++ sdi2021-304-spring/src/main/java/com/uniovi/services/InsertSampleDataService.java
@@ -13,37 +13,45 @@ import com.uniovi.entities.User;
 public class InsertSampleDataService {
 	@Autowired
 	private UsersService usersService;
-	
+
 	@Autowired
 	private ProfessorService professorService;
+
+	@Autowired
+	private RolesService rolesService;
 
 	@SuppressWarnings({ "serial", "rawtypes" })
 	@PostConstruct
 	public void init() {
-		
-		
+
 		for (int i = 0; i < 10; i++) {
-			Professor p1 = new Professor("dni"+i, "name"+i, "lastName"+i, "role"+i);
+			Professor p1 = new Professor("dni" + i, "name" + i, "lastName" + i, "role" + i);
 			professorService.addProfessor(p1);
 		}
-		
-		
-		
-		
+
 		User user1 = new User("99999990A", "Pedro", "Díaz");
 		user1.setPassword("123456");
+		user1.setRole(rolesService.getRoles()[0]);
+
 		User user2 = new User("99999991B", "Lucas", "Núñez");
 		user2.setPassword("123456");
+		user2.setRole(rolesService.getRoles()[0]);
+
 		User user3 = new User("99999992C", "María", "Rodríguez");
 		user3.setPassword("123456");
+		user3.setRole(rolesService.getRoles()[0]);
+
 		User user4 = new User("99999993D", "Marta", "Almonte");
 		user4.setPassword("123456");
+		user4.setRole(rolesService.getRoles()[1]);
 
 		User user5 = new User("99999977E", "Pelayo", "Valdes");
 		user5.setPassword("123456");
+		user5.setRole(rolesService.getRoles()[1]);
 
 		User user6 = new User("99999988F", "Edward", "Núñez");
 		user6.setPassword("123456");
+		user6.setRole(rolesService.getRoles()[2]);
 
 		Set user1Marks = new HashSet<Mark>() {
 			{
