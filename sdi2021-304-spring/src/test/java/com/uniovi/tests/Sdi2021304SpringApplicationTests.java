5bd0a73694f14efb89f01bb29e898b65b21504d2
UO271245 <71383208+UO271245@users.noreply.github.com>
2021-03-12 04:55:47 +0100

    SDI-2021-304-5.4-Casos Uso VistaHome.

--- sdi2021-304-spring/src/test/java/com/uniovi/tests/Sdi2021304SpringApplicationTests.java
+++ sdi2021-304-spring/src/test/java/com/uniovi/tests/Sdi2021304SpringApplicationTests.java
@@ -1,27 +1,38 @@
+
 package com.uniovi.tests;
+/*package com.uniovi;
 
-import static org.junit.Assert.fail;
-
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.FixMethodOrder;
 import org.junit.jupiter.api.Test;
-import org.junit.runners.MethodSorters;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi2021304SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}*/
+import static org.junit.Assert.*;
+import org.junit.*;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import com.uniovi.tests.pageobjects.PO_HomeView;
 import com.uniovi.tests.pageobjects.PO_Properties;
+
+import org.junit.runners.MethodSorters;
 //Ordenamos las pruebas por el nombre del método
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
-public class NotaneitorTests {
+//@SpringBootTest
+public class Sdi2021304SpringApplicationTests {
 
 	// En Windows (Debe ser la versión 65.0.1 y desactivar las actualizacioens
 	// automáticas)):
 	static String PathFirefox65 = "C:\\Program Files\\Mozilla Firefox\\firefox.exe";
-	static String Geckdriver024 = "C:\\Path\\geckodriver024win64.exe";
+	static String Geckdriver024 = "H:\\OneDrive\\OneDrive - Universidad de Oviedo\\Uni\\3Curso\\2ºSemestre\\Sistemas Distribuidos e Internet\\Laboratorio\\lab5\\PL-SDI-Sesión5-material\\geckodriver024win64.exe";
 	// En MACOSX (Debe ser la versión 65.0.1 y desactivar las actualizacioens
 	// automáticas):
 	// static String PathFirefox65 =
@@ -63,7 +74,6 @@ public class NotaneitorTests {
 	}
 
 
-
 	//PR01. Acceder a la página principal /
 	@Test
 	public void PR01() {
@@ -71,15 +81,23 @@ public class NotaneitorTests {
 	}
 	
 	
+	//PR02. OPción de navegación. Pinchar en el enlace Registro en la página home
+	@Test
+	public void PR02() {
+	PO_HomeView.clickOption(driver, "signup", "class", "btn btn-primary");
+	}
+	//PR03. OPción de navegación. Pinchar en el enlace Identificate en la página home
+	@Test
+	public void PR03() {
+	PO_HomeView.clickOption(driver, "login", "class", "btn btn-primary");
+	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	//PR04. OPción de navegación. Cambio de idioma de Español a Ingles y vuelta a Español
+	@Test
+	public void PR04() {
+	PO_HomeView.checkChangeIdiom(driver, "btnSpanish", "btnEnglish",
+	PO_Properties.getSPANISH(), PO_Properties.getENGLISH());
+	//SeleniumUtils.esperarSegundos(driver, 2);
+	}
 	
 }
